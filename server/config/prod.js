8351f5a0bbcea15f2d5964be2143ca3df2332a60
ruby723 <molly723@naver.com>
2024-10-28 01:13:06 +0900

    서버배포 시 NODE_ENV 환경변수 설정

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI:process.env.MONGO_URI
+    // mongoURI:process.env.MONGO_URI
+    mongoURI:'mongodb+srv://molly723:jCvWFebLQUPrmkaC@cluster0.lug7x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 }
